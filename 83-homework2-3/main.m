acb02ab2d7ef8387d75c9bc23b77b4708a5df741
Roberts <Roberts@RobertsMacBook-Pro.local>
2015-07-31 14:41:09 +0800

    Initial Commit

---
+++ 83-homework2-3/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  83-homework2-3
+//
+//  Created by Roberts on 15/7/31.
+//  Copyright (c) 2015年 iBokan Wisdom. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
